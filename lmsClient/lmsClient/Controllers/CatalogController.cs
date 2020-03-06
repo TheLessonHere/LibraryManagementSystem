1d225c6eb062e8aba4dedcdec66f98fef4e898df
TheLessonHere <48539448+TheLessonHere@users.noreply.github.com>
2020-03-05 17:52:07 -0700

    Styling added to catalog and asset page

--- lmsClient/lmsClient/Controllers/CatalogController.cs
+++ lmsClient/lmsClient/Controllers/CatalogController.cs
@@ -42,5 +42,27 @@ namespace lmsClient.Controllers
             // Return the model to the view
             return View(model);
         }
+
+        public IActionResult Detail(int id)
+        {
+            var asset = _assets.GetById(id);
+
+            var model = new AssetDetailModel
+            {
+                AssetId = id,
+                Title = asset.Title,
+                Year = asset.Year,
+                Cost = asset.Cost,
+                Status = asset.Status.Name,
+                ImageUrl = asset.ImageUrl,
+                AuthorOrDirector = _assets.GetAuthorOrDirector(id),
+                // Here a LibraryBranch Object is being returned so we only want the name on that object
+                CurrentLocation = _assets.GetCurrentLocation(id).Name,
+                DeweyCallNumber = _assets.GetDeweyIndex(id),
+                ISBN = _assets.GetIsbn(id),
+            };
+
+            return View(model);
+        }
     }
 }
